99824060c6e79f74ae446b4d60172bf248e3a4cd
Shihanul Islam Rian <shihanulriyan@gmail.com>
2026-06-12 12:45:33 +0600

    Add files via upload

---
+++ week-02-operators-control-flow/ArithmeticsDemo.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class ArithmeticsDemo {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        int num1, num2, result;
+
+        System.out.print("Enter first Number: ");
+        num1 = input.nextInt();
+
+        System.out.print("Enter second Number: ");
+        num2 = input.nextInt();
+
+        result = num1 + num2;
+        System.out.println("Sum = " + result);
+        
+        result = num1 - num2;
+        System.out.println("Sub = " + result);
+        
+        result = num1 * num2;
+        System.out.println("Multipication = " + result);
+        
+        double result2 = (double) num1 / num2;
+        System.out.println("Div = " + result2);
+        
+        result = num1 % num2;
+        System.out.println("Remainder = "+result);
+
+    }
+}
