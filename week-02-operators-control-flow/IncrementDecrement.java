2988be369b5f1dc17ddd7d153b62a30bdd2f614c
Shihanul Islam Rian <shihanulriyan@gmail.com>
2026-06-22 16:10:32 +0600

    Add files via upload

---
+++ week-02-operators-control-flow/IncrementDecrement.java
@@ -0,0 +1,15 @@
+public class IncrementDecrement{
+    public static void main(String[] args){
+
+        int a = 25;
+
+        // Increment
+        System.out.println(++a);  // 12 (aghe bare, pore print)
+        System.out.println(a++);  // 10 (aghe print, pore bare)
+        
+
+        // Decrement
+        System.out.println(--a);  // 10 (aghe kome, pore print)
+        System.out.println(a--);  // 12 (aghe print, pore kome)
+    }
+}
---
+++ week-02-operators-control-flow/UnaryDemo.java
@@ -0,0 +1,16 @@
+public class UnaryDemo {
+    
+    public static void main(String[] args) {
+        
+        int x = -10;
+        int result;
+
+        // +a
+
+        result = +x; //-10
+        System.out.println("result" + result);
+
+        result = -x; //10
+        System.out.println("result" + result);
+    }
+}
