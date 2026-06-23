844fcbed69964d9e9042ce3ecb9432d68729df03
Shihanul Islam Rian <shihanulriyan@gmail.com>
2026-06-23 17:46:36 +0600

    Add files via upload

---
+++ week-02-operators-control-flow/PositiveNegativeDemo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PositiveNegativeDemo {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        int num;
+        System.out.print("Enter any integer: ");
+        num = input.nextInt();
+
+        if(num>0)
+        {
+            System.out.println("Positive");
+        }
+        else
+        {
+            System.out.println("Negative");
+        }
+    }
+}
