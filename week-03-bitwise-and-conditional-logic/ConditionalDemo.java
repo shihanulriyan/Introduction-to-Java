0279e75d07c04e169888686a7e57a97586a0105d
Shihanul Islam Rian <shihanulriyan@gmail.com>
2026-06-28 01:09:54 +0600

    Add files via upload

---
+++ week-03-bitwise-and-conditional-logic/ConditionalDemo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ConditionalDemo {
+    
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        int num1, num2, large;
+        System.out.print("Enter any 2 Number: ");
+
+        num1 = input.nextInt();
+        num2 = input.nextInt();
+
+        large = (num1>num2)? num1 : num2;
+        System.out.println("Large Number "+large);
+    }
+}
