3c3f6bf25d3e38428f55a7149e8da7dcd5724357
Shihanul Islam Rian <shihanulriyan@gmail.com>
2026-06-28 01:34:58 +0600

    Add files via upload

---
+++ week-03-bitwise-and-conditional-logic/BitwiasOperator_Demo.java
@@ -0,0 +1,16 @@
+public class BitwiasOperator_Demo {
+    
+    public static void main(String[] args){
+
+        int a = 32;
+        int b = 12;
+
+        int c;
+
+        c = a>>3;
+        System.out.println("a>>3 "+c);
+
+        c = a<<3;
+        System.out.println("a<<3 "+c);
+    }
+}
---
+++ week-03-bitwise-and-conditional-logic/Bitwias_Operator.java
@@ -0,0 +1,21 @@
+public class Bitwias_Operator {
+    
+    public static void main(String[]args){
+
+        int a = 32;
+        int b = 12;
+
+        int c;
+
+        c = a & b;
+        System.out.println("a & b" + c);
+
+        c = a | b;
+        System.out.println("a | b" + c);
+        
+        c = a ^ b;
+        System.out.println("a ^ b" + c);
+         
+        
+    }
+}
