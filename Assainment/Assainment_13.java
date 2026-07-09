1dedf8a99f946f07909abb984fce42b4f4043f61
Shihanul Islam Rian <shihanulriyan@gmail.com>
2026-07-10 00:23:50 +0600

    Update Assainment_13.java

--- Assainment/Assainment_13.java
+++ Assainment/Assainment_13.java
@@ -1,18 +1,38 @@
 import java.util.Scanner;
+
 public class Assignment_13{
-   public static void main(String[] args){
 
-    try (Scanner input = new Scanner(System.in)){
-      System.out.print("start number: ");
-      int m = input.nextInt();
+    public static void main(String[] args){
 
-      System.out.print("end number: ");
-      int n = input.nextInt();
-      
-      int totalPalindromeNumber=0;
+        Scanner input = new Scanner(System.in);
 
-    
-     System.out.println("Total Palindrome numbers : "+totalPalindromeNumber);
+        int m, n, temp, r, sum, totalPalindromeNumber = 0;
+
+        System.out.print("Enter start number = ");
+        m = input.nextInt();
+
+        System.out.print("Enter end number = ");
+        n = input.nextInt();
+
+        System.out.println("Palindrome numbers are : ");
+
+        for(int i = m; i <= n; i++){
+
+            temp = i;
+            sum = 0;
+
+            while (temp != 0)
+            {
+                r = temp % 10;
+                sum = sum * 10 + r;
+                temp = temp / 10;
+            }
+            if (i == sum)
+            {
+                System.out.println(i);
+                totalPalindromeNumber++;
+            }
+        }
+        System.out.println("Total Palindrome numbers = " + totalPalindromeNumber);
     }
-  }
 }
