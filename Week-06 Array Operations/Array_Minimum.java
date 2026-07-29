b8bb7eeb5646f44392511ddfd897b44a31d6196d
Shihanul Islam Rian <shihanulriyan@gmail.com>
2026-07-29 23:14:39 +0600

    Add files via upload

---
+++ Week-06 Array Operations/Array_Minimum.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Array_Minimum{
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        double[] number = new double[5];
+        double sum = 0;
+
+        System.out.println("Please Enter Five Number = ");
+        for(int i=0; i<number.length; i++)
+        {
+            number[i] = input.nextDouble();
+        }
+
+        for(int i=0; i<number.length; i++)
+        {
+            sum = sum + number[i];
+        }
+        System.out.println("Sumation is = " + sum);
+        
+        double avg = sum / 5;
+        System.out.println("Average is : " + avg);
+        
+        double max = number[0];
+        double min = number[0];
+        for (int i = 1; i < 5; i++) {
+
+            if (max < number[i]) {
+                max = number[i];
+            }
+            if (min > number[i]) {
+                
+                min = number[i];
+            }
+                
+        }
+        System.out.println("Maximum is = " + max);
+        System.out.println("Minimum is = "+ min);
+    }
+}
