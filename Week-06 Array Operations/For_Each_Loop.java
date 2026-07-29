d667cf9dc070a0faec907fd3812ec420de64bb59
Shihanul Islam Rian <shihanulriyan@gmail.com>
2026-07-29 23:31:34 +0600

    Add files via upload

---
+++ Week-06 Array Operations/For_Each_Loop.java
@@ -0,0 +1,11 @@
+public class For_Each_Loop {
+    
+    public static void main(String[] args) {
+        
+        int[] num = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+
+        for (int x : num) {
+            System.out.println(x);
+        }
+    }
+}
